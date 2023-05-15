de5676584c2433ef18c967e6600a5ef8643eadeb
Sa Nguyen Thi Tuyet <nttsa@tma.com.vn>
2023-05-15 15:48:47 +0700

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,7 +12,12 @@ terraform {
       version = "3.4.3"
     }
   }
-  required_version = ">= 1.1.0"
+  cloud {
+    organization = "terraform-sanguyen"
+    workspaces {
+      name = "test-github-actions"
+    }
+  }
 }
 
 provider "aws" {
